ffa7af60651c7fba42b413c89e843da0e21a1d88
Chevelle <davebz@gmail.com>
2014-04-14 09:57:40 -0400

    Use own update-binary

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -2,7 +2,10 @@
 
 DIR=`pwd`
 
-echo "Copy GuardProvider"
 cd $DIR
+echo "Copy update-binary"
+cp -r other/update-binary out/target_files/OTA/bin/updater
+
+echo "Copy GuardProvider"
 cd ..
 cp -r miui/XHDPI/system/app/GuardProvider.apk $DIR/out/target_files/SYSTEM/app/
