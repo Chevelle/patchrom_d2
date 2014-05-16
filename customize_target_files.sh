fa237047183f8163a41f8a2e6c4f7c0b9e05c3b9
Chevelle <davebz@gmail.com>
2014-05-16 12:21:41 -0400

    Fix su

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -9,3 +9,7 @@ cp -r other/update-binary out/target_files/OTA/bin/updater
 echo "Copy GuardProvider"
 cd ..
 cp -r miui/XHDPI/system/app/GuardProvider.apk $DIR/out/target_files/SYSTEM/app/
+
+echo "Fix su"
+cd $DIR
+cp -r other/su out/target_files/SYSTEM/xbin/
