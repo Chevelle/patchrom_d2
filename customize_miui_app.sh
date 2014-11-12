9f61c8a75c32e257cb71272e9dc77d284cdab251
Chevelle <davebz@gmail.com>
2014-11-12 16:01:00 -0500

    Add to preferred network type cdma

---
+++ customize_miui_app.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# $1: dir for original miui app 
+# $2: dir for target miui app
+#
+
+XMLMERGYTOOL=$PORT_ROOT/tools/ResValuesModify/jar/ResValuesModify
+GIT_APPLY=$PORT_ROOT/tools/git.apply
+curdir=`pwd`
+
+function appendPart() {
+    for file in `find $1/smali -name *.part`
+    do
+		filepath=`dirname $file`
+		filename=`basename $file .part`
+		dstfile="out/$filepath/$filename"
+        cat $file >> $dstfile
+    done
+}
+
+if [ $1 = "Phone" ];then
+	$XMLMERGYTOOL $1/res/values $2/res/values
+fi
